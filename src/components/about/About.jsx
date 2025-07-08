4a15117d8d81c843bcaa4e55c6ec5944765911a0
MS-Cinti <szabadicintia@gmail.com>
2025-07-08 17:13:06 +0200

    changed the text of about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
           <h2>About me</h2>
           <p>After graduating and working for three years as a teacher, 
           I have decided to change my career and look for new challenges in IT.  In addition to various online courses, 
-          I have completed Codecool's Front-End Developer program which further increased my passion and motivation in working in this field. Since 2022, I am working as a Creative Developer at McCann Erickson where I had the opportunity to develop both my hard and soft skillset necessary for different projects. Overall, I am a highly motivated and easy-going person who always looks for opportunity to learn and develop as a collegue.
+          I have completed Codecool's Front-End Developer program which further increased my passion and motivation in working in this field. From 2022 until February 2025, I worked as a Creative Developer at McCann Erickson Agency in Budapest, where I had the opportunity to strengthen both my hard and soft skills across a variety of projects. Since March 2025, I have been participating in the FemaleTech program at Deutsche Telekom IT Solutions Hungary, where I am working on a new internal project that will serve as a knowledge-sharing platform. Overall, I am a highly motivated and easy-going person who always looks for opportunity to learn and develop as a colleague.
           </p>
         </div>
       </div>
